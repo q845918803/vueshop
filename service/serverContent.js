d3820c836d09a0c3faa8acb1c7bbafdb584bc205
Wap <war3hack@qq.com>
2019-08-10 21:55:53 +0800

    mongodb 连接错误处理

--- service/serverContent.js
+++ service/serverContent.js
@@ -2,20 +2,40 @@ const mongoose = require('mongoose')
 const db = 'mongodb://localhost/vueshop-db'
 
 exports.connect = ()=>{
-    //链接数据库
     mongoose.connect(db)
-    //增加数据库的监听事件
-    // 断开链接 /断开链接的回调函数
-    mongoose.connection.on('disconnected',()=>{
-
-        console.log(`数据库断开`)
-        mongoose.connect(db)
+    let maxConnectTimes = 0
+    return new Promise((res,rej)=>{
+            //链接数据库
+       
+        //增加数据库的监听事件
+        // 断开链接 /断开链接的回调函数
+        mongoose.connection.on('disconnected',()=>{
+                console.log('******数据库断开...******')
+            if(maxConnectTimes<=4){
+                maxConnectTimes ++
+                mongoose.connect(db)
+                console.log('正在尝试重连',new Date())
+            }else {
+                rej()
+                console.log(`数据库连接超时，请检查网络设置...`,new Date())
+            }
+            
+           
+        })
+        mongoose.connection.on('error',(err)=>{
+            console.log(`链接出错`,new Date())
+           if(maxConnectTimes<=4){
+            mongoose.connect(db)
+            console.log('正在尝试重连',new Date())
+           }else {
+               rej(err)
+            console.log('数据库连接超时，请检查配置文件...',new Date())
+           }
+            
+        })
+        mongoose.connection.once('open',()=>{
+            console.log('数据库连接成功!',new Date())
+        })
     })
-    mongoose.connection.on('error',()=>{
-        console.log(`链接出错`)
-        mongoose.connect(db)
-    })
-    mongoose.connection.once('open',()=>{
-        console.log('链接成功')
-    })
+  
 }
